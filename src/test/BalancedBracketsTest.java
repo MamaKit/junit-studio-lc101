53199e080800857ed129299c71e24ff9dce9a8ed
MamaKit <mama.kit.pyles@gmail.com>
2022-09-29 19:55:55 -0500

    Wrote 13 tests that pass.

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,65 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void onlyBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalse2() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void backwardBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void actualCorrectStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[does this bullshit work?]"));
+    }
+
+    @Test
+    public void unevenBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[]]"));
+    }
+
+    @Test
+    public void unevenBracketsReturnsFalse2() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]]]"));
+    }
+
+    @Test
+    public void curlyBraceReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("{]"));
+    }
+
+    @Test
+    public void curlyBraceReturnsFalse2() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[}"));
+    }
+
+    @Test
+    public void concatBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[" + "]"));
+    }
+
+    @Test
+    public void stringBreakReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch\nCode"));
+    }
+
+//    I have no fucking clue what's going on. So much lost. Didn't even do the reading yet.
 }
